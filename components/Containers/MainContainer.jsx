5ddd9f547b62306ae640e2aa2afc6a2cc69fbfdb
Korben <gaoyuan.hello@gmail.com>
2021-10-25 16:02:53 +1100

    add colors

--- components/Containers/MainContainer.jsx
+++ components/Containers/MainContainer.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const MainContainer = () => {
+const MainContainer = (props) => {
     return (
-        <div>
-
+        <div className={'grid grid-cols-12 gap-4'}>
+            {props.children}
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
     darkMode: false, // or 'media' or 'class'
     theme: {
+        colors: {
+            transparent: "transparent",
+            black: "#001219",
+            white: "#fff",
+            melon: "#FEC5BB",
+            pale_pink: "#FCD5CE",
+            misty_rose: "#FAE1DD",
+            isabelline: "#F8EDEB",
+            platinum: "#E8E8E4",
+            alabester: "#D8E2DC",
+            linen: "#ECE4DB",
+            champagne_pink: "#FFE5D9",
+            peach_puff: "#FFD7BA",
+            peach_crayolar: "#FEC89A"
+        },
         extend: {},
     },
     variants: {
